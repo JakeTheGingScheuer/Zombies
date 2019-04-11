09617e4e129fb5c7fceb1d89db5f12c0b809ce41
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-04-11 17:48:48 -0400

    added name field to survivor and refactored tests

--- src/main/java/zombies/Survivor.java
+++ src/main/java/zombies/Survivor.java
@@ -2,13 +2,20 @@ package zombies;
 
 public class Survivor {
 
+    private String name;
     private int wounds;
 
-    public Survivor() {
+
+    public Survivor(String name) {
+        this.name = name;
         this.wounds = 0;
     }
 
     public int getWounds() {
         return wounds;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
--- src/test/java/zombies/SurvivorTest.java
+++ src/test/java/zombies/SurvivorTest.java
@@ -8,9 +8,18 @@ public class SurvivorTest {
 
     @Test
     public void whenSurvivorIsCreatedItHas0Wounds() {
-        Survivor subject = new Survivor();
+        Survivor subject = new Survivor("");
         int expected = 0;
         int actual = subject.getWounds();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenSurvivorIsCreatedItHasAName() {
+        String testName = "KAARRRLLLL";
+        Survivor subject = new Survivor(testName);
+        String expected = testName;
+        String actual = subject.getName();
         assertEquals(expected, actual);
     }
 }
