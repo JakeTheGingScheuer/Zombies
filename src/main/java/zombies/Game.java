aa26a492e86f8011e0c89be1bc8913fed6b4aa3e
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-04-11 17:27:28 -0400

    refactored tests using mocks

--- src/main/java/zombies/Game.java
+++ src/main/java/zombies/Game.java
@@ -1,12 +1,22 @@
 package zombies;
 
+import java.util.List;
+
 public class Game {
 
+    private String gameLevel;
+    private List<Survivor> survivors;
+
+    public Game(List<Survivor> survivors){
+        this.survivors = survivors;
+        this.gameLevel = "Blue";
+    }
+
     public int getSurvivors() {
-        return 0;
+        return this.survivors.size();
     }
 
     public String getLevel() {
-        return "Blue";
+        return this.gameLevel;
     }
 }
--- src/test/java/zombies/GameTest.java
+++ src/test/java/zombies/GameTest.java
@@ -1,24 +1,43 @@
 package zombies;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class GameTest {
 
+    Game subject;
+
+    @Mock
+    Survivor mockSurvivor;
+
+    @Mock
+    List<Survivor> mockSurvivors;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        mockSurvivors.add(mockSurvivor);
+        subject = new Game(mockSurvivors);
+    }
+
+
     @Test
     public void whenGameIsCreatedItHas0Survivors() {
-        Game game = new Game();
         int expected = 0;
-        int actual = game.getSurvivors();
+        int actual = subject.getSurvivors();
         assertEquals(expected, actual);
     }
 
     @Test
     public void whenGameIsCreatedItIsAtBlueLevel() {
-        Game game = new Game();
         String expected = "Blue";
-        String actual = game.getLevel();
+        String actual = subject.getLevel();
         assertEquals(expected, actual);
     }
 }
