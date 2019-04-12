3e5a63699293519e7249fe40f863147b344d81ac
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-04-12 14:54:12 -0400

    can now add survivors

--- src/main/java/zombies/Game.java
+++ src/main/java/zombies/Game.java
@@ -1,7 +1,17 @@
 package zombies;
 
 public class Game {
+    private int numberOfSurvivors;
+
+    public Game() {
+        this.numberOfSurvivors = 0;
+    }
+
     public int getNumberOfSurvivors() {
-        return 0;
+        return this.numberOfSurvivors;
+    }
+
+    public void addSurvivor(Survivor survivor) {
+        numberOfSurvivors++;
     }
 }
--- src/test/java/zombies/GameTest.java
+++ src/test/java/zombies/GameTest.java
@@ -2,21 +2,36 @@ package zombies;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class GameTest {
 
     Game subject;
 
+    @Mock
+    Survivor survivor;
+
     @Before
     public void setUp() throws Exception {
+        initMocks(this);
         subject = new Game();
     }
 
     @Test
     public void gameStartsWith0Survivors() {
         int expected = 0;
+        int actual = subject.getNumberOfSurvivors();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gameCanAddSurvivor() {
+        subject.addSurvivor(survivor);
+
+        int expected = 1;
         int actual = subject.getNumberOfSurvivors();
         assertEquals(expected, actual);
     }
