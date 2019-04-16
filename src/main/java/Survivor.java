ed22713890d4787ebee0835402665a65b656c594
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-04-16 14:37:35 -0400

    survivor can now get injured

--- src/main/java/Survivor.java
+++ src/main/java/Survivor.java
@@ -1,11 +1,25 @@
 public class Survivor {
-    String name;
+    private String name;
+    private int wounds;
 
     public Survivor(String name) {
         this.name = name;
+        this.wounds = 0;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public int getNumberOfWounds() {
+        return 0;
+    }
+
+    public void injure() {
+        this.wounds++;
+    }
+
+    public boolean isAlive() {
+        return false;
     }
 }
--- src/test/java/SurvivorTest.java
+++ src/test/java/SurvivorTest.java
@@ -1,14 +1,32 @@
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+
 
 public class SurvivorTest {
 
+    Survivor survivor = new Survivor("Steve");
     @Test
     public void whenSurvivorIsCreatedItHasAName() {
-        Survivor survivor = new Survivor("Steve");
         assertEquals("Steve", survivor.getName());
+    }
+
+    @Test
+    public void whenSurvivorIsCreatedItHasNoWounds() {
+        int expected = 0;
+        int actual = survivor.getNumberOfWounds();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenSurvivorReceivesTwoWoundsItDies() {
+        survivor.injure();
+        survivor.injure();
+
+        assertFalse(survivor.isAlive());
+
     }
 }
